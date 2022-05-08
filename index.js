122ec1b39a3e04370bd5cdb9103ec877e7505c64
Haller125 <erokkabash1@gmail.com>
2022-05-08 17:32:44 +0500

    navbar by get requests

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const PORT = 5000;
+
+app.use(express.static(path.join(__dirname, 'Html')));
+app.use(express.static(path.join(__dirname, 'Css')));
+app.use(express.static(path.join(__dirname, 'Javascript')));
+app.use(express.static(path.join(__dirname, 'Images')));
+app.use('/Css', express.static(path.join(__dirname, 'Css')));
+app.use('/Javascript', express.static(path.join(__dirname, 'Javascript')));
+app.use('/Images', express.static(path.join(__dirname, '/Images')));
+
+app.get('/', (req, res) => {
+res.sendFile(`${__dirname}/Html/MainPage.html`);
+});
+
+app.get('/profile', (req, res) => {
+    res.sendFile(`${__dirname}/Html/profile.html`);
+});
+
+app.get('/registration', (req, res) => {
+    res.sendFile(`${__dirname}/Html/Registration.html`);
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(`${__dirname}/Html/notes.html`);
+});
+
+app.listen(PORT, () => {
+    console.log('Application listening on port ' + PORT);
+});
