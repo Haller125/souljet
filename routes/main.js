83f9b0576044c6d0e0bdd63ad091fd51115ac966
Haller125 <erokkabash1@gmail.com>
2022-05-20 21:08:14 +0500

    kinda normal roots

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,9 @@
-var express = require('express');
 const { Router } = require('express')
-const users = require('../models/users')
+const admins = require('../models/users')
+const users = require('../models/admins')
 const router = Router()
 const passwordValidator = require('password-validator');
+const users = require('../models/users');
 
 var schema = new passwordValidator();
 
@@ -35,34 +36,29 @@ router.get('/logInAdmin', (req, res) => {
   });
 
 router.post('/adminPage', async (req, res) => {
-    try {
-        await client.connect();
-        const database = client.db("test");
-        const test = database.collection("users");
-        let user = await test.find().toArray();
-        const collA = database.collection("admins");
-        let admin = await collA.findOne({'AdminName': req.body.name});
+        let users = await users.find({});
+        let admin = await admins.findOne({'AdminName': req.body.name});
         if(req.body.password == admin.PasswordAdmin){
-          res.render(`info`, {user: user});
+          res.render(`info`, {user: users});
         }
         else{
             res.send("invalid");
         }
-    }catch(e){
-        console.log(e);
-    } finally {
-        await client.close();
-    }
   });
 router.get('/contactus', (req, res) => {
       res.render('ContactUs');
   });
 
-router.post('/profile', (req, res) => {
+router.post('/registration', async (req, res) => {
       if(schema.validate(req.body.password)){
           res.render(`ConfirmPage`);
-          req.body.time = new Date().toISOString().slice(0, 19).replace('T', ' ');
-          addToDB(req.body);
+          let doc = {
+            username: req.body.name ,
+            email: req.body.email,
+            password: req.body.password,
+          }
+
+          await users.insertOne(doc);
       }else{
           res.send("Invalid password");
       };
@@ -73,98 +69,61 @@ router.get('/contact', (req, res) => {
   });
 
 router.post('/login', async (req, res) => {
-      try {
-          await client.connect();
-          const database = client.db("test");
-          const test = database.collection("users");
-          let user = await test.findOne({'email': req.body.email});
-          if(req.body.password === user.password){
-              res.render(`profile`, {
-                  username: user.name,
-                  email: user.email,
-                  time: new Date().toISOString().slice(0, 19).replace('T', ' '),
-              });
-          }else{
-              res.send('invalid');
-          };
-      }catch(e){
-          console.log(e);
-      } finally {
-          await client.close();
+      let user = await users.findOne({'email': req.body.email});
+      if(req.body.password === user.password){
+          res.render(`profile`, {
+              username: user.name,
+              email: user.email,
+              time: new Date().toISOString().slice(0, 19).replace('T', ' '),
+          });
+      }else{
+          res.send('invalid');
       }
   }); 
 
 router.get('/user/sortEmail', async function(req, res) {
-    await client.connect();
-    const database = client.db("test");
-    const coll = database.collection("users");
+    const coll = users.collection("users");
     //let email = req.params.email;
-    let user = await coll.find().sort({'email': 1}).toArray();
+    let user = await coll.find().sort({'email': 1});
     res.render('info', {user: user});
 });
 
 router.get('/user/sortName', async function(req, res) {
-    await client.connect();
-    const database = client.db("test");
-    const coll = database.collection("users");
-    let user = await coll.find().sort({'name': 1}).toArray();
+    const coll = users.collection("users");
+    let user = await coll.find().sort({'name': 1});
     res.render('info', {user: user});
-    client.close();
 });
 
 
-router.get('/user/delete:name', async function(req, res) {
-    await client.connect();
-    const database = client.db("test");
-    const coll = database.collection("users");
+router.get('/user/delete/:name', async function(req, res) {
     let name = req.params.name;
-    let user = await coll.deleteOne({name: name});
-    let users = await coll.find().toArray();
-    res.render('info', {user:users});
-    client.close();
+    await users.deleteOne({name: name});
+    let user1 = await users.find();
+    res.redirect('/adminPage');
 });
 ///user/show/{{name}}
-router.get('/user/show:name', async function(req, res) {
-    await client.connect();
-    const database = client.db("test");
-    const coll = database.collection("users");
+router.get('/user/show/:name', async function(req, res) {
     let name = req.params.name;
-    let user = await coll.findOne({name: name});
+    let user = await users.findOne({name: name});
     res.render('show', {user: user});
-    client.close();
 });
 
-router.get('/user/edit:name', async function(req, res) {
-    await client.connect();
-    const database = client.db("test");
-    const coll = database.collection("users");
+router.get('/user/edit/:name', async function(req, res) {
     let name = req.params.name;
-    let user = await coll.findOne({name: name});
+    let user = await users.findOne({name: name});
     res.render('edit', user);
-    client.close();
 });
 
-router.post('/user/edit:name', async function(req, res) {
-    await client.connect();
-    const database = client.db("test");
-    const coll = database.collection("users");
-    let user = req.body;
-    await coll.updateOne({name: user.name}, {$set: user});
-    let users = await coll.find().toArray();
-    res.render('info', {user: users});
-    client.close();
+router.post('/user/edit/:name', async function(req, res) {
+    await users.updateOne({name: user.name}, {$set: user});
+    res.redirect('/adminPage');
 });
 
 router.post('/user/add', async function(req, res) {
-    await client.connect();
-    const database = client.db("test");
-    const coll = database.collection("users");
     let user = req.body;
-    user.time = new Date().toISOString().slice(0, 19).replace('T', ' ');
-    await coll.insertOne(user);
-    let users = await coll.find().toArray();
-    res.render('info', {user: users});
-    client.close();
+    await users.insertOne(user);
+    let users = await users.find();
+    res.redirect('/adminPage');
 });
 
 router.get('/adding', async function(req,res){
